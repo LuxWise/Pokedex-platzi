d7f2a686feaab0b03e29d78ac5bec2c58733fb38
LuxWise <jcsanchez.martinez.2020@example.com>
2023-09-13 19:04:21 -0500

    muestra

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "CamiloLux12",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "CamiloLux12",
+  firstName: "Camilo",
+  lastName: "Sanchez",
+  email: "LuxWise@gmail.com",
+};
